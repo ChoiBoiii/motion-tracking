cd81ac80e256fb9fbe8ffe13878f0c9e3c7ccf13
ChoiBoiii <Isaakchoi@gmail.com>
2023-08-23 17:17:49 +1000

    .

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 ## IMPORT MODULES
-import os
 import cv2
 import pygame as py
+import pyautogui
+from os.path import abspath
 from Scripts.inputObj import InputObj
 from Scripts.setupFuncs import set_CWD_to_file
 from Scripts.camera import bindCam, getCamFrame, frameToPygameSurf
 
 
-## CONFIG ##
-WINDOW_NAME = 'Motion Capture'       # The title of the PyGame window
-WINDOW_DIMENSIONS = (1000, 700)      # Dimensions of the PyGame window in pixels. Format: [x, y]
-MAX_FPS = 30                         # The FPS cap of the main loop
-CAM_INDEX = 0                        # The index of the camera to get input from
-WINDOW_WIDTH = WINDOW_DIMENSIONS[0]  # The width of the PyGame window in pixels
-WINDOW_HEIGHT = WINDOW_DIMENSIONS[1] # The height of the PyGame window in pixels
-WINDOW_FLAGS = 0                     # The flags to create the PyGame window with
+## PYGAME CONFIG ##
+WINDOW_NAME = 'Motion Capture'   # The title of the PyGame window
+MAX_FPS = 30                     # The FPS cap of the main loop
+WINDOW_FLAGS = 0 | py.NOFRAME    # The flags to create the PyGame window with
 # ^ py.FULLSCREEN | py.NOFRAME | py.RESIZEABLE | py.HWSURFACE | py.DOUBLEBUF
+
+## CV2 CONFIG ##
+CAM_INDEX = 0                        # The index of the camera to get input from
+
+## PYAUTOGUI CONFIG ##
+pyautogui.PAUSE = 0                  # Pause in seconds after calls to pyautogui - Freezes whole program
+pyautogui.FAILSAFE = False           # Disable hotcorner program exit failsafe - WARNING: Can make it impossible to exit script
 
 
 ## MAKE WKDIR RELATIVE TO THIS SCRIPT ##
-set_CWD_to_file(absolutePath=os.path.abspath(__file__))
+set_CWD_to_file(absolutePath=abspath(__file__))
 
 
 ## MAIN
@@ -27,17 +31,20 @@ def main():
 
     ## Init PyGame
     py.init()
+    displayInfo = py.display.Info()
     py.display.set_caption(WINDOW_NAME)
     clock = py.time.Clock()
+    WINDOW_WIDTH = displayInfo.current_w 
+    WINDOW_HEIGHT = displayInfo.current_h
+    WINDOW_DIMENSIONS = (WINDOW_WIDTH, WINDOW_HEIGHT)
 
-    ## Init PyGame screen
     SCREEN = py.display.set_mode(size=WINDOW_DIMENSIONS, flags=WINDOW_FLAGS)
 
     ## Init input object for PyGame inputs
     Input = InputObj()
 
     ## Open webcam and bind input
-    cam = bindCam(CAM_INDEX)
+    # cam = bindCam(CAM_INDEX)
 
     ## Main loop
     run = True
@@ -53,10 +60,15 @@ def main():
         if Input.keys[py.K_ESCAPE]:
             run = False
 
-        ##
-        frame = getCamFrame(cam)
-        outSurf = frameToPygameSurf(frame, cv2.COLOR_BGR2RGB)
-        SCREEN.blit(outSurf, (0, 0))
+        ## Get input from cam 
+        # frame = getCamFrame(cam)
+        # outSurf = frameToPygameSurf(frame, cv2.COLOR_BGR2RGB)
+
+        ## Move mouse
+        pyautogui.moveTo(300, 300)
+
+        ## Render to screen surface
+        # SCREEN.blit(outSurf, (0, 0))
 
         ## Update display
         clock.tick(MAX_FPS)
