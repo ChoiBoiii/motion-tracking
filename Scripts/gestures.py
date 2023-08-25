bf5a91768fd828ea3a19df79e05f8fb103f36172
ChoiBoiii <Isaakchoi@gmail.com>
2023-08-25 17:30:46 +1000

    .

--- Scripts/gestures.py
+++ Scripts/gestures.py
@@ -1,43 +1,67 @@
 from Scripts.hands import HandMesh
+from typing import Union
 
 PINCH_DIST_INIT_THRESHOLD = 0.04
 PINCH_DISH_EXIT_THRESHOLD = 0.2
 
+def get_dist_3D(p1: tuple[float, float, float], p2: tuple[float, float, float]) -> float:
+    return (((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2) + ((p1[2] - p2[2]) ** 2)) ** 0.5
+
 def pinching_index(handMesh: HandMesh) -> bool:
-    ## Click
-    # minX = min(x for x, _, _ in rightHand.palm)
-    # minY = min(y for _, y, _ in rightHand.palm)
-    # minZ = max(z for _, _, z in rightHand.palm)
-    # maxX = max(x for x, _, _ in rightHand.palm)
-    # maxY = max(y for _, y, _ in rightHand.palm)
-    # maxZ = max(z for _, _, z in rightHand.palm)
-    # diffX = maxX - minX
-    # diffY = maxY - minY
-    # diffZ = maxZ - minZ
-    x1, y1, z1 = handMesh.index[2]
-    x2, y2, z2 = handMesh.thumb[2]
-    # dx, dy, dz = (x1 - x2) / diffX, (y1 - y2) * diffY, (z1 - z2) * diffZ
-    dx, dy, dz = (x1 - x2), (y1 - y2), (z1 - z2)
-    dist = (dx ** 2 + dy ** 2 + dz ** 2) ** 0.5
+    dist = get_dist_3D(handMesh.index[2], handMesh.thumb[2])
     return dist < PINCH_DIST_INIT_THRESHOLD
 
 def pinching_middle(handMesh: HandMesh) -> bool:
-    x1, y1, z1 = handMesh.middle[2]
-    x2, y2, z2 = handMesh.thumb[2]
-    dx, dy, dz = (x1 - x2), (y1 - y2), (z1 - z2)
-    dist = (dx ** 2 + dy ** 2 + dz ** 2) ** 0.5
+    dist = get_dist_3D(handMesh.middle[2], handMesh.thumb[2])
     return dist < PINCH_DIST_INIT_THRESHOLD
 
 def pinching_ring(handMesh: HandMesh) -> bool:
-    x1, y1, z1 = handMesh.ring[2]
-    x2, y2, z2 = handMesh.thumb[2]
-    dx, dy, dz = (x1 - x2), (y1 - y2), (z1 - z2)
-    dist = (dx ** 2 + dy ** 2 + dz ** 2) ** 0.5
+    dist = get_dist_3D(handMesh.ring[2], handMesh.thumb[2])
     return dist < PINCH_DIST_INIT_THRESHOLD
 
 def pinching_pinky(handMesh: HandMesh) -> bool:
-    x1, y1, z1 = handMesh.pinky[2]
-    x2, y2, z2 = handMesh.thumb[2]
-    dx, dy, dz = (x1 - x2), (y1 - y2), (z1 - z2)
-    dist = (dx ** 2 + dy ** 2 + dz ** 2) ** 0.5
+    dist = get_dist_3D(handMesh.pinky[2], handMesh.thumb[2])
     return dist < PINCH_DIST_INIT_THRESHOLD
+
+class Gestures:
+    
+    ## Enums
+    PINCHING_INDEX = 1
+    PINCHING_MIDDLE = 2
+    PINCHING_RING = 3
+    PINCHING_PINKY = 4
+
+    ## init
+    def __init__(self):
+
+        ## Create dicts to hold and poll gestures from
+        self.prevGestures = dict([])
+        self.gestures = dict([])
+
+        ## Add pinching gestures
+        self.prevGestures[Gestures.PINCHING_INDEX] = False
+        self.gestures[Gestures.PINCHING_INDEX] = False
+        self.prevGestures[Gestures.PINCHING_MIDDLE] = False
+        self.gestures[Gestures.PINCHING_MIDDLE] = False
+        self.prevGestures[Gestures.PINCHING_RING] = False
+        self.gestures[Gestures.PINCHING_RING] = False
+        self.prevGestures[Gestures.PINCHING_PINKY] = False
+        self.gestures[Gestures.PINCHING_PINKY] = False
+
+    ## Extract gestures from a hand mesh
+    def extract_gestrues(self, handMesh: HandMesh) -> None:
+
+        ## Shift
+        self.prevGestures = self.gestures
+
+        ## Extract pinching
+        thumbX, thumbY, thumbZ = handMesh.thumb[2]
+        x1, y1, z1 = handMesh.index[2]
+        # self.gestures[Gestures.PINCHING_INDEX] = 
+
+
+
+
+
+
+
